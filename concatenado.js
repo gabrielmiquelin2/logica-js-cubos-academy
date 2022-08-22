c7dda275f5d4e636570eca0e507ed910d3f1100e
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2022-08-22 18:02:01 -0300

    Update concatenado.js

--- concatenado.js
+++ concatenado.js
@@ -2,17 +2,20 @@
 
 const pessoa = {
   nome: "Gabriel",
-  idade:  11,
+  idade:  224,
   ehMaiorDeIdade: true,
   altura: 1.77
 };
 
+
+
 if (pessoa.idade < 18){
     console.log(pessoa.nome + " é menor de idade!")
 }else if (pessoa.idade < 60 ){
-    console.log("é adulta!")
+    console.log(`${pessoa.nome} é adulto`)
 }else{
-   console.log(" é idosa!")
+   console.log(`${pessoa.nome} é um idoso de $
+    {pessoa.idade} anos.`);
 }
 
 
