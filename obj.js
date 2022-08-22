fda9c65298a1e946d543a611d2b0369bffa04d91
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2022-08-22 17:33:52 -0300

    Create obj.js

---
+++ obj.js
@@ -0,0 +1,9 @@
+//OBIJETOS {} = conjunto de variaves
+
+const pessoa = {
+   nome: "Gabrieç",
+   idade:  17,
+   ehMaiorDeIdade: false,
+   altura: 1.77
+};
+console.log(pessoa.altura);     //(.) serve para ir para um próximo nivel ou seja se quero pegar uma variavel especifica no caso "altra"
