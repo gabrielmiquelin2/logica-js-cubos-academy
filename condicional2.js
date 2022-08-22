9b53bd09aee333aaaaa2a811542e28eba6c2e714
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2022-08-22 17:51:47 -0300

    Create condicional2.js

---
+++ condicional2.js
@@ -0,0 +1,24 @@
+const pessoa = {
+  nome: "Gabrieç",
+  idade:  65,
+  ehMaiorDeIdade: true,
+  altura: 1.77
+};
+
+if (pessoa.idade < 18){
+    console.log("A Pessoa é menor de idade!")
+}else if (pessoa.idade < 60 ){
+    console.log("A pessoa é adulta!")
+}else{
+   console.log("A pessoa é idosa!")
+}
+
+
+// comparadores
+// >= maior ou igual
+// <= menour ou igual
+// > maior
+// < menor
+// == igualdade
+// != Diferente
+// === verifica o conteudo e tipo ex:se é numero ou texto etc...
