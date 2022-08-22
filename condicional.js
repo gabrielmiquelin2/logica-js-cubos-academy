9a956e256e8242c03978e71608847906a139909f
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2022-08-22 17:45:20 -0300

    Create condicional.js

---
+++ condicional.js
@@ -0,0 +1,22 @@
+const pessoa = {
+  nome: "Gabrieç",
+  idade:  18,
+  ehMaiorDeIdade: true,
+  altura: 1.77
+};
+
+if (pessoa.idade == 18){
+   console.log("Entro no if")
+}else{
+  console.log("entrou no else")
+}
+
+
+// comparadores
+// >= maior ou igual
+// <= menour ou igual
+// > maior
+// < menor
+// == igualdade
+// != Diferente
+// === verifica o conteudo e tipo ex:se é numero ou texto etc...
