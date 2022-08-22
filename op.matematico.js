26b0cb4988d1fe417f137db6147ad4bb6425dfa8
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2022-08-22 16:53:36 -0300

    Create op.matematico.js

---
+++ op.matematico.js
@@ -0,0 +1,6 @@
+//OPERADORES MATEMÁTICOS
+
+const resultado = (5 + 4) * 3;  //As aspas() serve para indicar qual vai somar primeiro no caso 5+4
+const resto = 5 % 4;
+console.log(resultado)
+console.log(resto);
