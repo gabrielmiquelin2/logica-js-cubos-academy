dc90fa633624f0226bc251d3b23a9643b5985aaa
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2022-08-23 16:03:56 -0300

    Create arrays.js

---
+++ arrays.js
@@ -0,0 +1,6 @@
+//ARRAYS ou Vetores [] = são um conjunto de variaves ordenadas
+
+const listaDeComprars = ["Arroz", "Carne", "Batata"]
+
+console.log(listaDeComprars[0]);//esse (0) é o número do indice de cada item da lista 0,1,2,3...
+console.log(listaDeComprars[2]);
