b98ceb397b7c14282e1ceac2d347a4eaa179ee00
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2022-08-23 18:01:37 -0300

    Create funcoes2.js

---
+++ funcoes2.js
@@ -0,0 +1,8 @@
+//FUNÇÔES COM PARAMETROS
+                //variavel "nome"(só existe ou melhor só funciona dentro dessa chaves {} ali em baixo)
+function cumprimentarPessoa(nome) {
+    console.log("Olá, " + nome);
+}
+
+cumprimentarPessoa("Gabriel")   //chamei a função
+cumprimentarPessoa("Josefina")   //chamei a função
