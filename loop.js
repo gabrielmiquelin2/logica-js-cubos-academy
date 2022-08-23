3883f9a325510914fbe8c7572e2542aef864ae85
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2022-08-23 16:11:03 -0300

    Create loop.js

---
+++ loop.js
@@ -0,0 +1,9 @@
+//Lopps
+
+const listaDeComprars = ["Arroz", "Carne", "Batata"]
+//IMPRIMIR NUMEROS DE 0 a 100!
+let numero = 0
+while(numero <= 100)/*dentro das aspas coloca condição*/{
+    console.log(numero);
+    numero++;
+}
