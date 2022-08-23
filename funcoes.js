5a7acc9ef97aa7d91f7f041f6435eec06b25d4e6
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2022-08-23 17:56:10 -0300

    Create funcoes.js

---
+++ funcoes.js
@@ -0,0 +1,9 @@
+//FUNÇÔES são basicamente bloco de códigos que vc pode criar para evitar ficar repetindo a msm lógica em varios lugares
+
+function imprimirOlaNaTela(){
+   console.log("Olá")
+}
+
+imprimirOlaNaTela() //chamei a funcao para aparecer na tela
+console.log("Oi!")
+imprimirOlaNaTela();
