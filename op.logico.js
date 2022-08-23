f4c40937475e9c0dcb4615e0d49325abea12e0cc
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2022-08-23 15:30:56 -0300

    Create op.logico.js

---
+++ op.logico.js
@@ -0,0 +1,21 @@
+//OPERADORES LÓGICOS
+
+//EXBIR OS REQUISITOS MINIMOS PARA PODER BRINCAR
+//Alutra:1.40 e idade:12
+
+//meu algoritmo
+const pessoa = {
+  nome: "Gabriel",
+  idade: 13,
+  altura: 1.90,
+}
+
+if(pessoa.idade >= 12){
+  console.log("Pode Brincar!")
+}else{
+  console.log("Não pode brincar!")
+} if (pessoa.idade >= 1.40){
+  console.log("Você tem alutra mínima!")
+}else{
+  console.log("Você NÂO tem altura mínima estabelecida!")
+}
