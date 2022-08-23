476f1a8fce5f8b3793830d4600ff4cff59f94d7d
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2022-08-23 18:10:48 -0300

    dias da semana ex

---
+++ ex.js
@@ -0,0 +1,27 @@
+//EXERCICIO
+//Que dia da semana será daqui a X dias?
+//obs:hpje é domingo
+
+function diaDaSemana(dias){
+   const resto = dias % 7;
+
+   if(resto == 0){
+    console.log("Domingo")
+   }else if (resto == 1){
+    console.log("Segunda")
+   }else if (resto == 2){
+    console.log("Terça")
+   }else if (resto == 3){
+    console.log("Quarta")
+   }else if (resto == 4){
+    console.log("Quinta")
+   }else if (resto == 5){
+    console.log("Sexta")
+   }else if (resto == 6){
+    console.log("Sabado")
+   }
+}
+
+
+
+diaDaSemana(300)
