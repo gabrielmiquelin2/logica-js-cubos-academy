1b9aa67f6e1c2b7318f317e1b3db12a493515740
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2022-08-23 16:14:37 -0300

    Create loop2.js

---
+++ loop2.js
@@ -0,0 +1,9 @@
+//Lopps
+
+const listaDeComprars = ["Arroz", "Carne", "Batata", "Vinagre", "Peixe"]
+//IMPRIMIR NUMEROS DE 0 a 100!
+let indice = 0
+while(indice < listaDeComprars.length)/*dentro das aspas coloca condição*/{
+    console.log(listaDeComprars[indice]);
+    indice++;
+}
