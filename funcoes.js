bf3c52eec206777eb3b800a212f1027bd958ba54
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2022-09-05 16:48:33 -0300

    Update funcoes.js

--- funcoes.js
+++ funcoes.js
@@ -1,7 +1,8 @@
 //FUNÇÔES são basicamente bloco de códigos que vc pode criar para evitar ficar repetindo a msm lógica em varios lugares
 
 function imprimirOlaNaTela(){
-   console.log("Olá")
+   console.log("Olá")  //declarei a função ou seja estou dizendo o que quero
+                       //  que seja executado
 }
 
 imprimirOlaNaTela() //chamei a funcao para aparecer na tela
