173e65206094889374959796021fe577b82d1fa4
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2022-09-05 14:14:24 -0300

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -4,3 +4,4 @@ const listaDeComprars = ["Arroz", "Carne", "Batata"]
 
 console.log(listaDeComprars[0]);//esse (0) é o número do indice de cada item da lista 0,1,2,3...
 console.log(listaDeComprars[2]);
+
